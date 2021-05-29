c254d66af2726b55b046a8ead949bcbde3700c1e
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-28 17:04:06 -0700

    Add logger

--- src/org/dhsdev/flowerknight/Main.java
+++ src/org/dhsdev/flowerknight/Main.java
@@ -1,5 +1,8 @@
 package org.dhsdev.flowerknight;
 
+import org.dhsdev.flowerknight.util.Logger;
+import org.dhsdev.flowerknight.util.Severity;
+
 /**
  * Main driver code for the FlowerKnight game. Right now this has all of the
  * windowing code stuffed inside - later it should be moved somewhere else.
@@ -7,14 +10,18 @@ package org.dhsdev.flowerknight;
  */
 public class Main {
 
+    public static final Logger logger = new Logger();
     /**
      * Main driver code for the application.
      * @param args command line arguments - currently unused
      */
     public static void main(String[] args) {
         FlowerKnight.init();
+        logger.log("Main initialization", Severity.NORMAL);
+
         FlowerKnight.mainloop();
         FlowerKnight.exit();
+        logger.log("Main closed", Severity.NORMAL);
     }
 
 }
