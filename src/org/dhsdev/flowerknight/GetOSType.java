fc0fe524e6102c573ebfd69ab7ad6272cf92fb4f
Tom <treez.zhang@gmail.com>
2021-05-29 13:42:01 -0700

    getOSType

--- src/org/dhsdev/flowerknight/GetOSType.java
+++ src/org/dhsdev/flowerknight/GetOSType.java
@@ -8,16 +8,20 @@ import java.util.Locale;
  */
 public class GetOSType {
 
+    private GetOSType() {
+        throw new IllegalStateException("GetOSType Class");
+    }
+
     /**
      * Actual function to get OS
      * @return The OS of the system
      */
     public static OSType getOSType() {
-        String OS = System.getProperty("os.name").toLowerCase();
+        String os = System.getProperty("os.name").toLowerCase();
 
-        if (OS.contains("win")) return OSType.WINDOWS;  // Windows
-        if (OS.contains("mac")) return OSType.MACOS;    // Mac OS
-        if (OS.contains("linux")) return OSType.LINUX;  // Linux
+        if (os.contains("win")) return OSType.WINDOWS;  // Windows
+        if (os.contains("mac")) return OSType.MACOS;    // Mac OS
+        if (os.contains("linux")) return OSType.LINUX;  // Linux
 
         // If the function has not returned yet, the OS is none of the
         // above, so we can safely say that the OS is OSType.OTHER
