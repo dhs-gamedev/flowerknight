5c377095d2817e6a3e155087bd897f59b40b61fd
adamhutchings <adam.abahot@gmail.com>
2021-06-02 23:21:39 -0700

    Dim brightness

--- src/shader/spotlight_frag.glsl
+++ src/shader/spotlight_frag.glsl
@@ -10,6 +10,6 @@ uniform float time;
 void main() {
     fragColor = texture(texture_sampler, outTexCoord);
     float x = 0.5 + cos(time) / 2, y = 0.5 + sin(time) / 2;
-    float dist = distance(vec3(x, y, 0.5), vec3(outTexCoord, 0));
+    float dist = distance(vec3(x, y, 1.0), vec3(outTexCoord, 0));
     fragColor *= 1 / (dist * dist);
 }
