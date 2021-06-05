4859fc16412dc5dafce65e36d41cc378a289fbd7
adamhutchings <adam.abahot@gmail.com>
2021-06-04 17:33:54 -0700

    Add width and height uniforms

--- src/shader/trivial_vert.glsl
+++ src/shader/trivial_vert.glsl
@@ -5,7 +5,22 @@ layout (location=1) in vec2 texCoord;
 
 out vec2 outTexCoord;
 
+uniform float width, height;
+
 void main() {
+
     gl_Position = vec4(position, 1.0);
+
+    // Width and height are the width and height of the window. We want to have
+    // everything rendered in a square, so everything is rendered in the largest
+    // square possible. To do this, we scale the larger coordinate space down to
+    // the smaller one.
+    if (width < height) {
+        gl_Position.y *= (width / height);
+    } else {
+        gl_Position.x *= (height / width);
+    }
+
     outTexCoord = texCoord;
+
 }
